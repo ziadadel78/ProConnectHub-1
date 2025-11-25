af39d06a32e6df45e6d6d999e347f74ece273887
ziadadel78 <118855912+ziadadel78@users.noreply.github.com>
2025-11-25 23:37:23 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -57,19 +57,14 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
+  
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+  
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
